ae15381f638927c9688a6e9b7b6de9962c7ab609
wangyinghan <wangx6@gmail.com>
2020-07-26 17:19:59 +0100

    asdf

--- src/user/js/controller/product.controller.js
+++ src/user/js/controller/product.controller.js
@@ -10,6 +10,21 @@ module.exports = [
 function(location, timeout, s, rs, $controller, win, productService, bagService) {
 	s.productService = productService;
 
+	s.showProducts = false;
+	s.showRevertHeader = false;
+	s.showHeader = true;
+	s.onMouseover = function() {
+		s.showProducts = true;
+		s.showRevertHeader = true;
+		s.showHeader = false;
+	}
+
+	s.onMouseLeaveProducts = function() {
+		s.showProducts = false;
+		s.showRevertHeader = false;
+		s.showHeader = true;
+	};
+
 	s.onClickItem = (item) => {
 		location.path('/product-detail').search({id: item.id});
 	}
